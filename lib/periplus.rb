d52e15dcda3f1b4202f7dce32b22be1df180172d
Ryan Crum <ryan.j.crum@gmail.com>
2011-07-26 16:25:20 -0400

    Add pushpins param to class methods

--- lib/periplus.rb
+++ lib/periplus.rb
@@ -20,13 +20,13 @@ module Periplus
     Location.new (get url)
   end
 
-  def self.route_map_url(key, waypoints, options = {})
+  def self.route_map_url(key, waypoints, pushpins = [], options = {})
     request = Request.new key
-    request.route_map_url waypoints, options
+    request.route_map_url waypoints, pushpins, options
   end
 
-  def self.address_map_url(key, address, options = {})
+  def self.address_map_url(key, address, pushpins = [], options = {})
     request = Request.new key
-    request.address_map_url address, options
+    request.address_map_url address, pushpins, options
   end
 end
