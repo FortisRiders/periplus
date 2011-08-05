c14ef466c5a9d77750a46fc89494bdfe93a5a22f
Ryan Crum <ryan.j.crum@gmail.com>
2011-08-05 14:36:19 -0400

    Add name to location result

--- lib/periplus/location.rb
+++ lib/periplus/location.rb
@@ -5,13 +5,15 @@ module Periplus
     attr_accessor :address
     attr_accessor :confidence
     attr_accessor :entity_type
+    attr_accessor :name
 
     def parse
       super()
 
       point = @primary_resource["point"]
       @latitude, @longitude = point["coordinates"]
-      @address=  @primary_resource["address"]
+      @name = @primary_resource["name"]
+      @address =  @primary_resource["address"]
       @confidence = @primary_resource["confidence"].downcase.to_sym
       @entity_type = @primary_resource["entityType"].downcase.to_sym
     end
